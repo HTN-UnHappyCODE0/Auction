4acd33dee048bef9a36292f1ac9e898601d8c891
legal <2051063590@e.tlu.edu.vn>
2024-04-12 15:06:38 +0700

    fix router

--- aution/src/router/router.js
+++ aution/src/router/router.js
@@ -3,12 +3,10 @@ import { onMounted } from "vue";
 import { initFlowbite } from "flowbite";
 import auction from "../views/auction.vue";
 import home from "../views/home.vue";
-import artwork from "../views/artwork.vue";
 import live from "../views/live.vue";
 import cart from "@/views/cart.vue";
 import ProductDetail from "../views/productDetail.vue";
 import DefaultLayout from "../default/DefaultLayout.vue";
-import painting from "@/views/painting.vue";
 import Category from "../views/category.vue";
 
 const router = createRouter({
